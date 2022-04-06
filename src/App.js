5006ef908b2c7bec733e096d9a1ababd5347ae26
fahd23 <fahdpno@gmail.com>
2022-04-06 13:20:51 +0530

    FEAT: Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Notes taking App</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../App.css";
+import "./navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar flex jtfy-c-spce-arnd">
+      <h1>Notes App</h1>
+      <button className="btn warning">Add Notes</button>
+    </nav>
+  );
+}
