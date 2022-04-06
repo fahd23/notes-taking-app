550a3b905f6fa3e9db9292eacbb766f872d1db58
fahd23 <fahdpno@gmail.com>
2022-04-06 16:10:34 +0530

    FEAT: Added Router

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,24 +1,25 @@
 import React from "react";
 import "./sidebar.css";
-import "../../App.css";
+import "App.css";
+import { Link } from "react-router-dom";
 import { RiPagesLine } from "react-icons/ri";
 import { MdOutlineLabel, MdArchive, MdDelete } from "react-icons/md";
 
 export default function Sidebar() {
   return (
     <div className="sidebar">
-      <p className="sidebar-btn">
+      <Link to="/" className="sidebar-btn">
         <RiPagesLine /> Notes
-      </p>
-      <p className="sidebar-btn">
+      </Link>
+      <Link to="/label" className="sidebar-btn">
         <MdOutlineLabel /> Label
-      </p>
-      <p className="sidebar-btn">
+      </Link>
+      <Link to="/archive" className="sidebar-btn">
         <MdArchive /> Archive
-      </p>
-      <p className="sidebar-btn">
+      </Link>
+      <Link to="/delete" className="sidebar-btn">
         <MdDelete /> Delete
-      </p>
+      </Link>
     </div>
   );
 }
